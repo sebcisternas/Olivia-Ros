4b8503e5cb5bcb3b7f190e20e8aef5f989fbd946
Sebastian <sebcisternas@gmail.com>
2024-05-20 00:09:47 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+var suma = document.getElementById("suma"); 
+var resta = document.getElementById("resta"); 
+var division = document.getElementById("division"); 
+var mul= document.getElementById("multiplicacion");2
+
+
+
+var num1 = parseFloat(prompt("Ingrese Numero 1"));
+var num2 = parseFloat(prompt("Ingrese Numero 2"));
+
+var suma = num1+num2
+var mul = num1*num2
+var resta = num1-num2
+var div = num1/num2
+
+
+
+
+
+
+
